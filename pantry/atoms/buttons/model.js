7763b1d34507a53d9972a5b2d1eb80be8af162de
Elyse Holladay <eholladay@rmn.com>
2017-03-23 16:14:27 -0500

    commit test pantry for roux workshop

---
+++ pantry/atoms/buttons/model.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = exports = {
+	primary: {
+		type: 'primary',
+		text: 'Primary'
+	},
+	secondary: {
+		type: 'secondary',
+		text: 'Secondary'
+	},
+	text: {
+		type: 'text',
+		text: 'Text Button'
+	}
+};
---
+++ pantry/atoms/typography/ingredient.md
@@ -0,0 +1,7 @@
+# Typography
+
+Typography styles.
+
+Primary: Google's Crimson Text, serif. For some headlines, body copy.
+
+Secondary: Google's Rubik, all caps, sans-serif. For buttons, headlines.
